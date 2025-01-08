c3f35c4c022ac36689dd04750899938eb43a8740
Halimat Yaya <halimatyayas@gmail.com>
2025-01-08 15:15:45 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.scss';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+function App() {
+
+
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+      <Route/>
+
+     </Routes>
+     
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
