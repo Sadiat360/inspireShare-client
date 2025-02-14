a5ea494b90c372117af6208d8aa235f9f114c1ec
Halimat Yaya <halimatyayas@gmail.com>
2025-02-13 18:39:00 -0700

    categoryform.sccs typo corrected

--- src/components/CategoryForm/CategoryForm.jsx
+++ src/components/CategoryForm/CategoryForm.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
-import './categoryForm.scss'
+import './CategoryForm.scss'
 import axios from 'axios';
 
 
